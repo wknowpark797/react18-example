d3513131c460d0e41b2f650718320e50d9ced1b8
wknowpark797 <wknowpark797@gmail.com>
2023-08-02 11:46:58 +0900

    기존의 CSR, SSR 방식의 개념 정리

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 // import App from './App';
-import App2 from './App2';
+// import App2 from './App2';
+import App3 from './App3';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 	// <React.StrictMode>
-	<App2 />
+	<App3 />
 	// </React.StrictMode>
 );
